b47dcb71fbc1b28526311c7a655e0ee2528f4182
jimohsegun1 <jimohsegunj@gmail.com>
2024-06-24 16:52:45 +0100

    frontend: descriptionbox

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>
+            Welcome to Your Store Name, where fashion meets affordability. 
+            Explore our curated collection of stylish apparel, crafted with quality materials for comfort and durability. From casual essentials to elegant evening wear, Your Store Name offers a range of options to suit every occasion and personal style. Enjoy exceptional service, affordable luxury, and exclusive offers as you discover your new wardrobe favorites. Join our fashion range of options to suit every occasion and personal style. Enjoy exceptional service, affordable luxury, and exclusive offers as you discover your new wardrobe favorites. Join our fashion community today and start shopping confidently at Your Store Name.
+            </p>
+            <p>
+            E-commerce to Your Store Name, where fashion meets affordability. 
+            Explore our curated collection of stylish apparel, crafted with quality materials for comfort and durability. From casual essentials to elegant evening wear, Your Store Name offers a range of options to suit every occasion and personal style. Enjoy exceptional service, affordable luxury, and exclusive offers as you discover your new wardrobe favorites. Join our fashion range of options to suit every occasion and personal style. Enjoy exceptional service, affordable luxury, and exclusive offers as you discover your new wardrobe favorites. Join our fashion community today and start shopping confidently at Your Store Name.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
